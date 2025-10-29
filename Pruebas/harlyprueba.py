817a4708a1ab1767c3af0f066560600e916f8aac
Harly <cafinharly@gmail.com>
2025-10-28 22:48:30 -0500

    Actualizando harly prueba con menu interactivo

--- Pruebas/harlyprueba.py
+++ Pruebas/harlyprueba.py
@@ -1,14 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from Harlybreria import Cuantitativa, Cualitativa
+from Harlybreria import Cuantitativa, Cualitativa, menu
 
 url = "https://raw.githubusercontent.com/RaulAM22/TED-XD/refs/heads/main/paises1957.csv"
 df = pd.read_csv(url)
 
-analisis_cuanti = Cuantitativa(df, "esperanza_de_vida")
-analisis_cuanti.resumen()
-analisis_cuanti.graficar()
-
-analisis_cuali = Cualitativa(df, "continente")
-analisis_cuali.tabla_frecuencia()
-analisis_cuali.graficar()
+menu(df)
